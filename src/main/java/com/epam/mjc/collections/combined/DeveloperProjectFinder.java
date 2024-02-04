02e56f827611016fb0a02cf5d5ab1fbc5d0d77a0
Igor Bugaiov <bugayov.igor@nixsolutions.com>
2024-02-04 13:16:58 +0100

    st1_mod4_collections_t_final_try_1

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,18 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> res = projects.keySet()
+                .stream()
+                .filter(k -> projects.get(k).contains(developer))
+                .collect(Collectors.toList());
+
+        if (res.stream().allMatch(v -> v.length() == res.get(0).length()))
+            Collections.reverse(res);
+
+        return res;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,21 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> res = new HashMap<>();
+
+        sourceMap.keySet().forEach(k ->
+                res.put(k.length(),
+                        sourceMap.keySet().stream()
+                                .filter(v -> v.length() == k.length())
+                                .collect(Collectors.toSet()))
+        );
+
+        return res;
     }
 }
